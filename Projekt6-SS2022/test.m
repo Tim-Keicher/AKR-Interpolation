eca22f9c7691dda79a3cfc87afa541a4819a22d6
Tim-Keicher <keicher04@gmail.com>
2023-12-21 10:32:08 +0100

    call example from function (.m file)

--- Projekt6-SS2022/test.m
+++ Projekt6-SS2022/test.m
@@ -1,11 +1,29 @@
-function [x, y] = test(steps)
-    x = 0:steps:(360/180)*pi;
-    y = sin(x);
-
+function test(app)
+    clc;
+    
     p_punkte = [-40 40;
-            -40 65;
-             40 65;
-             40 15;
-            -40 15;
-            -40 40]';
+                -40 65;
+                 40 65;
+                 40 15;
+                -40 15;
+                -40 40]';
+    
+    axis(app, [-100 100 -30 70]);
+    plot(app, p_punkte(1, :), p_punkte(2, :), 'k-'); % Rechteck zeichnen
+    hold(app, 'on');
+    
+    x = -40:0.5:40;
+    y = ones(1, length(x)) * 65;
+    
+    % plot(app.robotDrive,x,y);
+    
+    h = animatedline(app, 'Marker', 'o', 'Color', 'r');
+    
+    for k = 1:length(x)
+        clearpoints(h);
+        addpoints(h, x(k), y(k));
+        drawnow();
+    end
+    
+    hold(app, 'off');
 end
