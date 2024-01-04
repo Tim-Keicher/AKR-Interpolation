d9e27a40464629fe1d15b20686c6b6bc882e3eac
heiko <cocowa99ahoo.de>
2024-01-04 17:43:34 +0100

    added a new file for Visualisation of Scara-Robot

--- Projekt6-SS2022/Visiualisierung_Roboter.m
+++ Projekt6-SS2022/Visiualisierung_Roboter.m
@@ -18,47 +18,94 @@ close all
 % config(4).JointPosition   Rotation des Manipulator Gelenks    Gelenk J3
 % show(robot,randomConfiguration(robot)); % Erzeugt zufällige Roboter Position
 %%
-winkel1 = winkel(:,1);
-winkel2= winkel(:,2);
-if size(winkel1,1) ~= size(winkel2,1)
-    disp("ERROR!!!!!!")
+% winkel1 = winkel(:,1);
+% winkel2= winkel(:,2);
+% if size(winkel1,1) ~= size(winkel2,1)
+%     error("ERROR!!!!!!")
+% end
+% tpts = 0:4;
+% sampleRate = size(winkel1,1) ;
+% tvec = tpts(1):1/sampleRate:tpts(end);
+% numSamples = length(tvec);
+% 
+% robot = loadrobot('omronEcobra600',DataFormat='column'); % quanserQArm 
+% 
+% gen_waypoints = zeros(4, size(winkel1, 1)-1);
+% 
+% for i = 1:size(winkel1, 1)
+%     gen_waypoints(:, i) = [0; winkel1(i, 1) - pi/2; winkel2(i, 1)-pi/2 ;  0];
+% end
+% %rng default
+% 
+% % %% Original
+% % robot.homeConfiguration robot.homeConfiguration
+% % frankaWaypoints = gen_waypoints
+% % frankaTimepoints = linspace(tvec(1),tvec(end),4); 
+% % [q,qd] = trapveltraj(frankaWaypoints,numSamples);
+% % %%
+% 
+% frankaWaypoints = gen_waypoints
+% %frankaWaypoints=[[0, -1.53248023105472, -0.0390500675991217, 0]', [0, -0.888766093087712, 1.14699686008123, 0]']
+% %frankaTimepoints = linspace(tvec(1),tvec(end),160); 
+% [q,qd] = trapveltraj(frankaWaypoints,numSamples);
+% 
+% figure
+% set(gcf,'Visible','on');
+% rc = rateControl(sampleRate);
+% trueb = 0
+% while trueb < 0
+% 
+%     for i = 1:numSamples
+%         tNow= numSamples(i);
+%         show(robot,q(:,i),FastUpdate=true,PreservePlot=false);
+%         waitfor(rc);
+%         configNow = interp1(tTask,stateTask(:,1:numJoints),tNow);
+%         poseNow = getTransform(robot,configNow,endEffector);
+%         taskSpaceMarker = plot3(poseNow(1,4),poseNow(2,4),poseNow(3,4),'b.','MarkerSize',20);
+%         drawnow;
+%     end
+%     trueb = trueb +1
+% end
+
+%robot = loadrobot("omronEcobra600");
+% showdetails(robot)
+% config = homeConfiguration(robot)
+% show(robot);
+%
+winkelJ1 = winkel(:,1);
+winkelJ2 = winkel(:,2);
+
+if size(winkelJ1,1) ~= size(winkelJ2,1)
+    disp("ERROR!")
+    return
 end
 tpts = 0:4;
-sampleRate = size(winkel1,1) ;
+sampleRate = size(winkelJ1,1) ;
 tvec = tpts(1):1/sampleRate:tpts(end);
 numSamples = length(tvec);
 
-robot = loadrobot('quanserQArm',DataFormat='column'); % quanserQArm
+robot = loadrobot('omronEcobra600',DataFormat='column'); %frankaEmikaPanda,    quanserQArm
 
-gen_waypoints = zeros(4, size(winkel1, 1)-1);
+gen_waypoints = zeros(4, size(winkelJ1, 1)-1);
 
-for i = 1:size(winkel1, 1)
-    gen_waypoints(:, i) = [0; winkel1(i, 1)-pi/2;winkel2(i, 1)-pi/2 ;  0];
+for i = 1:size(winkelJ1, 1)
+    gen_waypoints(:, i) = [winkelJ1(i, 1);winkelJ2(i, 1) ; 0;  0];
+
 end
-%rng default
+frankaWaypoints = gen_waypoints;
+[q,~] = trapveltraj(frankaWaypoints,numSamples);
 
-% %% Original
-% robot.homeConfiguration robot.homeConfiguration
-% frankaWaypoints = gen_waypoints
-% frankaTimepoints = linspace(tvec(1),tvec(end),4); 
-% [q,qd] = trapveltraj(frankaWaypoints,numSamples);
-% %%
-
-frankaWaypoints = gen_waypoints
-%frankaWaypoints=[[0, -1.53248023105472, -0.0390500675991217, 0]', [0, -0.888766093087712, 1.14699686008123, 0]']
-%frankaTimepoints = linspace(tvec(1),tvec(end),160); 
-[q,qd] = trapveltraj(frankaWaypoints,numSamples);
 
 figure
 set(gcf,'Visible','on');
 rc = rateControl(sampleRate);
 trueb = 0
-while trueb < 2
+while trueb < 5
 
-for i = 1:numSamples
-    
-    show(robot,q(:,i),FastUpdate=true,PreservePlot=false);
-    waitfor(rc);
-end
-trueb = trueb +1
+    for i = 1:numSamples
+
+        show(robot,q(:,i),FastUpdate=true,PreservePlot=false);
+        waitfor(rc);
+    end
+    trueb = trueb +1
 end
---
+++ Projekt6-SS2022/visualisation_scara.m
@@ -0,0 +1,43 @@
+function [] = visualisation_scara(winkel)
+
+winkelJ1 = winkel(:,1);
+winkelJ2 = winkel(:,2);
+
+if size(winkelJ1,1) ~= size(winkelJ2,1)
+    disp("ERROR!")
+    return
+end
+tpts = 0:4;
+sampleRate = size(winkelJ1,1) ;
+tvec = tpts(1):1/sampleRate:tpts(end);
+numSamples = length(tvec);
+
+robot = loadrobot('omronEcobra600',DataFormat='column'); %quanserQArm
+
+gen_waypoints = zeros(4, size(winkelJ1, 1)-1);
+
+for i = 1:size(winkelJ1, 1)
+    gen_waypoints(:, i) = [winkelJ1(i, 1);winkelJ2(i, 1) ; 0;  0];
+
+end
+frankaWaypoints = gen_waypoints;
+[q,~] = trapveltraj(frankaWaypoints,numSamples);
+
+
+figure
+set(gcf,'Visible','on');
+rc = rateControl(sampleRate);
+trueb = 0
+while trueb < 5
+
+    for i = 1:numSamples
+
+        show(robot,q(:,i),FastUpdate=true,PreservePlot=false);
+        waitfor(rc);
+    end
+    trueb = trueb +1
+end
+
+
+end
+
