7a4b6439ce616bd12ef95727a623b841705579a9
Tim-Keicher <keicher04@gmail.com>
2024-01-12 12:21:06 +0100

    fix bug: grid was switching from layout 3x2 to 2x2 (title in axes)

--- Projekt6-SS2022/designAxes.m
+++ Projekt6-SS2022/designAxes.m
@@ -12,31 +12,31 @@ function designAxes(app)
     % Neuanordnung der Sichtbaren Axen
     cnt = 1;
     if app.BewegungssimulationCheckBox.Value
-        app.robotSimUIAxes.Layout.Row = 1;
+        app.robotSimUIAxes.Layout.Row = 1 + 1;
         app.robotSimUIAxes.Layout.Column = 1;
         cnt = cnt + 1;
     end
     if app.WinkelVerlaufCheckBox.Value
-        app.angleUIAxes.Layout.Row = 1;
+        app.angleUIAxes.Layout.Row = 1 + 1;
         app.angleUIAxes.Layout.Column = cnt;
         cnt = cnt + 1;
     end
     if app.GeschwVerlaufCheckBox.Value
         if mod(cnt,2) == 0
-            app.velUIAxes.Layout.Row = cnt / 2;
+            app.velUIAxes.Layout.Row = cnt / 2 + 1;
             app.velUIAxes.Layout.Column = 2;
         else
-            app.velUIAxes.Layout.Row = round(cnt/2);
+            app.velUIAxes.Layout.Row = round(cnt/2) + 1;
             app.velUIAxes.Layout.Column = 1;
         end
         cnt = cnt + 1;
     end
     if app.BeschlVerlaufCheckBox.Value
         if mod(cnt,2) == 0
-            app.accUIAxes.Layout.Row = cnt / 2;
+            app.accUIAxes.Layout.Row = cnt / 2 + 1;
             app.accUIAxes.Layout.Column = 2;
         else
-            app.accUIAxes.Layout.Row = round(cnt/2);
+            app.accUIAxes.Layout.Row = round(cnt/2) + 1;
             app.accUIAxes.Layout.Column = 1;
         end
         cnt = cnt + 1;
@@ -46,19 +46,19 @@ function designAxes(app)
     switch (cnt - 1)
         case 1
             % Eine sichtbare Axes: Ganzes Layout verwenden
-            app.GridLayoutLeft.RowHeight = {'1x', '0x'};
+            app.GridLayoutLeft.RowHeight = {'0.2x', '1x', '0x'};
             app.GridLayoutLeft.ColumnWidth = {'1x', '0x'};
         case 2
             % Zwei sichtbare Axes: 2x1 Layout
-            app.GridLayoutLeft.RowHeight = {'1x', '0x'};
+            app.GridLayoutLeft.RowHeight = {'0.2x', '1x', '0x'};
             app.GridLayoutLeft.ColumnWidth = {'1x', '1x'};
         case 3
             % Drei sichtbare Axes: 2x2 Layout
-            app.GridLayoutLeft.RowHeight = {'1x', '1x'};
+            app.GridLayoutLeft.RowHeight = {'0.2x', '1x', '1x'};
             app.GridLayoutLeft.ColumnWidth = {'1x', '1x'};
         case 4
             % Vier sichtbare Axes: 2x2 Layout
-            app.GridLayoutLeft.RowHeight = {'1x', '1x'};
+            app.GridLayoutLeft.RowHeight = {'0.2x', '1x', '1x'};
             app.GridLayoutLeft.ColumnWidth = {'1x', '1x'};
         otherwise
             warning("Visible Axes: case not available!");
